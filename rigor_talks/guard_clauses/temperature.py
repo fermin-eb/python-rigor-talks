08bd5ac6a037652de60c10d25065c8ab984e005b
Fermin <fermin@eventbrite.com>
2019-12-30 11:15:19 +0100

    Create directories

--- rigor_talks/guard_clauses/temperature.py
+++ rigor_talks/guard_clauses/temperature.py
@@ -6,13 +6,10 @@ class TemperatureNegativeException(Exception):
 class Temperature():
 
     def __init__(self, measure: int):
-        self.__set_measure(measure)
-
-    def __set_measure(self, measure):
-        self.__check_measure_is_positive(measure)
+        self._check_measure_is_positive(measure)
         self.__measure = measure
 
-    def __check_measure_is_positive(self, measure: int):
+    def _check_measure_is_positive(self, measure: int):
         if (measure < 0):
             raise TemperatureNegativeException("Measure should be postive")
 
