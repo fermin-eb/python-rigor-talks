7e85c4cae850e519c246dea52e678b1cc606165f
Fermin <fermin@eventbrite.com>
2019-12-30 11:13:09 +0100

    refactor: extract to a function

--- rigor_talks/guard_clauses/temperature.py
+++ rigor_talks/guard_clauses/temperature.py
@@ -6,13 +6,13 @@ class TemperatureNegativeException(Exception):
 class Temperature():
 
     def __init__(self, measure: int):
-        # Guard clause: simplify code and raise exceptions
-        # Passed guard clauses, code is valide
-        self._check_measure_is_positive(measure)
+        self.__set_measure(measure)
 
+    def __set_measure(self, measure):
+        self.__check_measure_is_positive(measure)
         self.__measure = measure
 
-    def _check_measure_is_positive(self, measure: int):
+    def __check_measure_is_positive(self, measure: int):
         if (measure < 0):
             raise TemperatureNegativeException("Measure should be postive")
 
