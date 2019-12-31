4269b1eaa8d1e317e12cfc4986bd22630946fe90
Fermin <fermin@eventbrite.com>
2019-12-31 10:52:35 +0100

    added folders and files

---
+++ rigor_talks/test_class/temperature.py
@@ -0,0 +1,26 @@
+
+class TemperatureNegativeException(Exception):
+    @classmethod
+    def from_measure(cls, measure: int):
+        return cls(f"Measure should be postive: {measure}")
+
+
+class Temperature():
+
+    def __init__(self, measure: int):
+        self.__set_measure(measure)
+
+    def __set_measure(self, measure):
+        self.__check_measure_is_positive(measure)
+        self.__measure = measure
+
+    def __check_measure_is_positive(self, measure: int):
+        if (measure < 0):
+            raise TemperatureNegativeException.from_measure(measure)
+
+    @classmethod
+    def take(cls, measure: int):
+        return cls(measure)
+
+    def measure(self):
+        return self.__measure
---
+++ test/test_class/test_temperature.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from rigor_talks.named_constructors.temperature import (
+    Temperature,
+    TemperatureNegativeException
+)
+
+
+class TestTemperatureTest(TestCase):
+
+    def test_trytoCreateANonValidTemperature(self):
+        with self.assertRaises(TemperatureNegativeException):
+            Temperature.take(-1)
+
+    def test_tryToCreateAValidTemperature(self):
+        measure = 18
+        self.assertEqual(
+            measure,
+            (Temperature.take(measure)).measure()
+        )
+
+    def test_trytoCreateAValidTemperatureWithNamedConstructor(self):
+        measure = 18
+        self.assertEqual(
+            measure,
+            (Temperature.take(measure)).measure()
+        )
